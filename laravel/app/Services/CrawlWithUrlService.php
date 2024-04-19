10349bd123ef14dcbff4ae5313ad0c0760fa242c
will.wang <will.wang@piapp.com.tw>
2024-04-19 15:25:22 +0800

    update service

--- laravel/app/Services/CrawlWithUrlService.php
+++ laravel/app/Services/CrawlWithUrlService.php
@@ -9,11 +9,20 @@ class CrawlWithUrlService
     {
         $client = new Client();
         $crawler = $client->request('GET', $url);
-
-        $title = $crawler->filter('title')->text();
-        $description = $crawler->filter('meta[name="description"]')->attr('content');
-        $body = $crawler->filter('body')->text();
         $today = date("Y-m-d H:i:s");
+        $title = $description = $body = 'Not available';
+    
+        if ($crawler->filter('title')->count()) {
+            $title = $crawler->filter('title')->text();
+        }
+    
+        if ($crawler->filter('meta[name="description"]')->count()) {
+            $description = $crawler->filter('meta[name="description"]')->attr('content');
+        }
+
+        if ($crawler->filter('body')->count()) {
+            $body = $crawler->filter('body')->text();
+        }
 
         return [
             'crawledResults' => [
