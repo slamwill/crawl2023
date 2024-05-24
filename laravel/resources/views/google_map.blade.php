b46d24110b8d4eb2cad6070d1ec45fbcddc89605
will.wang <will.wang@piapp.com.tw>
2024-05-24 17:22:25 +0800

    map

---
+++ laravel/resources/views/google_map.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google Map</title>
+    <style>
+        #map2, #map1 {
+            height: 500px;
+            width: 100%;
+            border: 0;
+        }
+    </style>
+</head>
+<body>
+    <h1>My Google Map1</h1>
+    <iframe id="map1" 
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14461.596945227324!2d121.5496191967875!3d25.020521928650517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442abe10238cb87%3A0x8c0ca929f18c0d23!2z5Y-w5YyXMTAx!5e0!3m2!1szh-TW!2stw!4v1716542273106!5m2!1szh-TW!2stw" 
+        width="600" 
+        height="450" 
+        style="border:0;" 
+        allowfullscreen="" 
+        loading="lazy" 
+        referrerpolicy="no-referrer-when-downgrade">
+    </iframe>
+    <h1>My Google Map2</h1>
+    <iframe id="map2" 
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.204982146185!2d144.9575797153177!3d-37.81720917975171!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0x5045675218ce7e0!2sMelbourne%20VIC%2C%20Australia!5e0!3m2!1sen!2sus!4v1593569985712!5m2!1sen!2sus" 
+        width="600" 
+        height="450" 
+        frameborder="0" 
+        style="border:0;" 
+        allowfullscreen="" 
+        aria-hidden="false" 
+        tabindex="0">
+    </iframe>
+</body>
+</html>
